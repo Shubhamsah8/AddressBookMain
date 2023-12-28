cb732fb29171cf5ac3d62fddbc87fe1dba82643b
Shubham Shah <138213993+Shubhamsah8@users.noreply.github.com>
2023-12-28 17:50:15 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -24,30 +24,29 @@ public class Main {
         String email = scanner.nextLine();
         
         Contact contact = new Contact(firstName, lastName, address, city, state, zip, phoneNumber, email);
-        addressBook.addContact(newContact);
+        addressBook.addContact(contact);
         addressBook.displayContacts();
 
-        //Editng the contact
 
-        System.out.println("Enter your full name to edit: ");
-        String fullNameToEdit = scanner.nextLine();
+
         System.out.println("Enter First name: ");
+        String firstName = scanner.nextLine();
+        System.out.println("Enter Last name: ");
+        String lastName = scanner.nextLine();
         System.out.println("Enter the address: ");
-        String newAddress = scanner.nextLine();
+        String address = scanner.nextLine();
         System.out.println("Enter the city: ");
-        String newCity = scanner.nextLine();
+        String city = scanner.nextLine();
         System.out.println("Enter the state: ");
-        String newState = scanner.nextLine();
+        String state = scanner.nextLine();
         System.out.println("Enter the zip: ");
-        String newZip = scanner.nextLine();
+        String zip = scanner.nextLine();
         System.out.println("Enter the phone number: ");
-        String newPhoneNumber = scanner.nextLine();
+        String phoneNumber = scanner.nextLine();
         System.out.println("Enter the email: ");
-        String newEmail = scanner.nextLine();
-
-        addressBook.editContact(fullNameToEdit, newAddress, newCity, newState, newZip, newPhoneNumber, newEmail);
-
-        //Displaying all contacts after editing
-        System.out.println("All contacts after editing:");
+        String email = scanner.nextLine();
+        
+        Contact contact1 = new Contact(firstName, lastName, address, city, state, zip, phoneNumber, email);
+        addressBook.addContact(contact1);
         addressBook.displayContacts();
 }
