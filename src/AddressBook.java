b0501a149d55a47b82e9b8262bd24197deaa2f3b
Shubham Shah <138213993+Shubhamsah8@users.noreply.github.com>
2023-12-28 17:29:09 +0530

    Update AddressBook.java

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,38 +1,36 @@
 import java.util.*;
 
-interface ContactOperations {
-
-    //Defined various abstract methods
-    void addContact(Contact contact);
-
-    List<Contact> listContacts();
-}
-
 // Class AddressBook that implements the interface
-class AddressBook implements ContactOperations {
+class AddressBook {
 
     // Initialize the variables
-    private String name;
     private List<Contact> contacts;
 
-    public AddressBook(String name) {
-        this.name = name;
+    public AddressBook() {
         this.contacts = new ArrayList<>();
     }
 
-    // Override the method of Interface
-    @Override
     public void addContact(Contact contact) {
         contacts.add(contact);
     }
 
-    @Override
-    public List<Contact> listContacts() {
-        return new ArrayList<>(contacts);
+    public void editContact(String fullName, String address, String city, String state, String zip, String phoneNumber, String email){
+        for(Contact contact: contacts){
+            if(contact.getFullName().equals(fullName)){
+                contact.editDetails(address, city, state, zip, phoneNumber, email);
+                System.out.println("Contact Updated Successfully!);
+            }
+        }
+        System.out.println("Contact not found with the given name");
+    }
+    
+    //Display the contacts
+
+    public void displayAllContacts(){
+        for(Contact contact: contacts){
+            contact.displayDetails();
+        }
     }
 
-    public String getName() {
-        return name;
-    }
 }
 
