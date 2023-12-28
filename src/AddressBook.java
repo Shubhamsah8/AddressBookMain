4cc1ab1ec9f0ff6b0fe8f9a47e3491127145cfcd
Shubham Shah <138213993+Shubhamsah8@users.noreply.github.com>
2023-12-28 17:47:57 +0530

    Update AddressBook.java

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -14,19 +14,6 @@ class AddressBook {
         contacts.add(contact);
     }
 
-    public void editContact(String fullName, String address, String city, String state, String zip, String phoneNumber, String email){
-        boolean contactFound = false;
-        for(Contact contact: contacts){
-            if(contact.getFullName().equals(fullName)){
-                contact.editDetails(address, city, state, zip, phoneNumber, email);
-                System.out.println("Contact Updated Successfully!);
-                contactFound = true;
-                break;
-            }
-        }
-        if(!contactFound){
-            System.out.println("Contact not found with the given name");
-    }
     
     //Display the contacts
 
