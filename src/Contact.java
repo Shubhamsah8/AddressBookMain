fc71be9cd51265927456c2a04ded888435a18649
Shubham Shah <138213993+Shubhamsah8@users.noreply.github.com>
2023-12-28 17:33:49 +0530

    Update Contact.java

--- src/Contact.java
+++ src/Contact.java
@@ -20,15 +20,11 @@ class Contact {
     }
 
     // Getters
-    public String getFirstName() {
-        return firstName;
+    public String getName() {
+        return firstName+" "+lastName;
     }
 
-    public String getLastName() {
-        return lastName;
-    }
-
-        public String address(){
+     public String address(){
         return address;
     }
 
@@ -44,6 +40,16 @@ class Contact {
         return email;
     }
 
+    
+    public void displayDetails(){
+        System.out.println("Name: "+getFullName());
+        System.out.println("Address: "+getAddress());
+        System.out.println("City: "+getCity());
+        System.out.println("State: "+getState());
+        System.out.println("Zip: "+getZip());
+        System.out.println("Phone Number: "+getPhoneNumber());
+        System.out.println("Email: "+getEmail());
+    }
     //Edit the existing details
     public void editDetails(String address, String city, String state, String zip, String phoneNumber, String email){
         this.address = address;
@@ -53,3 +59,4 @@ class Contact {
         this.phoneNumber = phoneNumber;
         this.email = email;
 }
+
