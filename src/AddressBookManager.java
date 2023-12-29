00242e40c52e53f5d1ce974890cabe54429a89ea
Shubham Shah <138213993+Shubhamsah8@users.noreply.github.com>
2023-12-29 10:22:57 +0530

    Update AddressBookManager.java

--- src/AddressBookManager.java
+++ src/AddressBookManager.java
@@ -11,9 +11,14 @@ class AddressBookManager {
     }
 
     // Method to put things in address book with name
-    public void addAddressBook(String name) {
-        AddressBook addressBook = new AddressBook(name);
-        addressBooks.put(name, addressBook);
+     public void addAddressBook(String name) {
+        if (!addressBooks.containsKey(name)) {
+            AddressBook addressBook = new AddressBook(name);
+            addressBooks.put(name, addressBook);
+            System.out.println("Address book '" + name + "' added successfully.");
+        } else {
+            System.out.println("Address book with the name '" + name + "' already exists.");
+        }
     }
 
     // Getters of return type AddressBook class
