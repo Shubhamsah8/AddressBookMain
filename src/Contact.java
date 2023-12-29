ad10204ecb2bb7a7f7dc8ae8a249a4aa28735cba
Shubham Shah <138213993+Shubhamsah8@users.noreply.github.com>
2023-12-29 09:02:09 +0530

    Update Contact.java

--- src/Contact.java
+++ src/Contact.java
@@ -8,6 +8,7 @@ class Contact {
     private String phoneNumber;
     private String email;
 
+    // Constructor
     public Contact(String firstName, String lastName, String address, String city, String state, String zip, String phoneNumber, String email) {
         this.firstName = firstName;
         this.lastName = lastName;
@@ -20,36 +21,11 @@ class Contact {
     }
 
     // Getters
-    public String getName() {
-        return firstName+" "+lastName;
+    public String getFirstName() {
+        return firstName;
     }
 
-     public String address(){
-        return address;
+    public String getLastName() {
+        return lastName;
     }
-
-    public String zip(){
-        return zip;
-    }
-
-    public String phoneNumber(){
-        return phoneNumber;
-    }
-
-    public String email(){
-        return email;
-    }
-
-    
-    public void displayDetails(){
-        System.out.println("Name: "+getFullName());
-        System.out.println("Address: "+getAddress());
-        System.out.println("City: "+getCity());
-        System.out.println("State: "+getState());
-        System.out.println("Zip: "+getZip());
-        System.out.println("Phone Number: "+getPhoneNumber());
-        System.out.println("Email: "+getEmail());
-    }
-    
 }
-
